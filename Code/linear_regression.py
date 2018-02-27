7bdcafa837a956fb0f1e9a41769e1074e769ad6d
Lan Bai <bailan0506@gmail.com>
2018-02-27 15:46:22 -0600

    Edit linear regression and svm for new dataset

--- Code/linear_regression.py
+++ Code/linear_regression.py
@@ -1,13 +1,13 @@
 
 # coding: utf-8
 
-# In[15]:
+# In[2]:
 
 
 #cd E:\18Spring\CS839\Stage1\CS839_1\Code
 
 
-# In[16]:
+# In[3]:
 
 
 import os
@@ -19,9 +19,9 @@ from sklearn import datasets, linear_model
 from sklearn.model_selection import train_test_split
 
 
-fid = './Input.txt';
+fid = './train.txt';
 f = open(fid);
-X = []; Y = [];
+X_train = []; Y_train = [];
 while 1:
 	x = f.readline();
 	if not x:
@@ -32,20 +32,40 @@ while 1:
 	y = f.readline();
 	y = y.rstrip('\n');
 	y = float(y);
-	X.append(x);
-	Y.append(y);
+	X_train.append(x);
+	Y_train.append(y);
 f.close();
 
+
+fid = './test.txt';
+f = open(fid);
+X_test = []; Y_test = [];
+while 1:
+	x = f.readline();
+	if not x:
+		break;
+	x = x.rstrip('\n');
+	x = x.split(',');
+	x = [float(x[i]) for i in range(len(x))];
+	y = f.readline();
+	y = y.rstrip('\n');
+	y = float(y);
+	X_test.append(x);
+	Y_test.append(y);
+f.close();
 # Split the data into training/testing sets
 #X_train = X[:-1130]
 #X_test = X[-1130:]
 
 # Split the targets into training/testing sets
-Y = [int(y>0) for y in Y]
+#Y = [int(y>0) for y in Y]
 #split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+#X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 # Create linear regression object
+
+Y_train = [int(y>0) for y in Y_train]
+Y_test = [int(y>0) for y in Y_test]
 regr = linear_model.LinearRegression()
 
 
@@ -60,24 +80,28 @@ Y_predict = regr.predict(X_test)
 
 
 
-# In[17]:
+# In[6]:
 
 
 from sklearn.model_selection import cross_val_score
 from sklearn import metrics
-threshold = 0.73
+threshold = 0.5
 Y_predict = [int(y>threshold) for y in Y_predict]
 p = metrics.precision_score(Y_test, Y_predict)
 r = metrics.recall_score(Y_test,Y_predict)
 print p, r
 
 
-# In[18]:
+# In[14]:
 
 
 #cross validation
+from sklearn.utils import shuffle
+#X_train, Y_train = shuffle(X_train,Y_train)
 train_size = len(X_train)
+print train_size
 subset_size = train_size/5
+print subset_size
 p_list = []
 r_list = []
 f1_list = []
@@ -87,13 +111,16 @@ for i in range(0, 5):
     cv_train_Y = Y_train[0:i*subset_size] +  Y_train[(i+1)*subset_size:]
     cv_test_X = X_train[i*subset_size:(i+1)*subset_size]
     cv_test_Y = Y_train[i*subset_size:(i+1)*subset_size]
+    #print len(cv_train_X), len(cv_train_Y),len(cv_test_X),len(cv_test_Y)
     regr.fit(cv_train_X, cv_train_Y)
     cv_predict = regr.predict(cv_test_X)
     cv_predict = [int(y>threshold) for y in cv_predict]
+    #print cv_test_Y[0:100]
+    #print cv_predict[0:100]
     p_list.append(metrics.precision_score(cv_test_Y, cv_predict))
     r_list.append(metrics.recall_score(cv_test_Y, cv_predict))
     f1_list.append(metrics.f1_score(cv_test_Y, cv_predict))
-print p_list
-print r_list
-print f1_list
+print np.average(p_list)
+print np.average(r_list)
+print np.average(f1_list)
 
--- Code/svm_lb.py
+++ Code/svm_lb.py
@@ -1,13 +1,13 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 #cd E:\18Spring\CS839\Stage1\CS839_1\Code
 
 
-# In[8]:
+# In[12]:
 
 
 import os
@@ -19,9 +19,9 @@ from sklearn import datasets, linear_model,svm
 from sklearn.model_selection import train_test_split
 
 
-fid = './Input.txt';
+fid = './train.txt';
 f = open(fid);
-X = []; Y = [];
+X_train = []; Y_train = [];
 while 1:
 	x = f.readline();
 	if not x:
@@ -32,21 +32,56 @@ while 1:
 	y = f.readline();
 	y = y.rstrip('\n');
 	y = float(y);
-	X.append(x);
-	Y.append(y);
+	X_train.append(x);
+	Y_train.append(y);
 f.close();
+
+
+fid = './test.txt';
+f = open(fid);
+X_test = []; Y_test = [];
+while 1:
+	x = f.readline();
+	if not x:
+		break;
+	x = x.rstrip('\n');
+	x = x.split(',');
+	x = [float(x[i]) for i in range(len(x))];
+	y = f.readline();
+	y = y.rstrip('\n');
+	y = float(y);
+	X_test.append(x);
+	Y_test.append(y);
+f.close();
+
+# fid = './Input.txt';
+# f = open(fid);
+# X = []; Y = [];
+# while 1:
+# 	x = f.readline();
+# 	if not x:
+# 		break;
+# 	x = x.rstrip('\n');
+# 	x = x.split(',');
+# 	x = [float(x[i]) for i in range(len(x))];
+# 	y = f.readline();
+# 	y = y.rstrip('\n');
+# 	y = float(y);
+# 	X.append(x);
+# 	Y.append(y);
+# f.close();
 
 # Split the data into training/testing sets
 #X_train = X[:-1130]
 #X_test = X[-1130:]
 
 # Split the targets into training/testing sets
-Y = [int(y>0) for y in Y]
+#Y = [int(y>0) for y in Y]
 #split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+#X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 # Create linear regression object
-clf = svm.SVC(kernel='rbf', C=100, random_state=0, tol=1e-12)
+clf = svm.SVC(kernel='rbf', C=1, random_state=0, tol=1e-14)
 
 
 
@@ -60,14 +95,15 @@ clf = svm.SVC(kernel='rbf', C=100, random_state=0, tol=1e-12)
 
 
 
-# In[9]:
+# In[13]:
 
 
 from sklearn.model_selection import cross_validate
 from sklearn.metrics import recall_score
-scoring = ['precision_macro', 'recall_macro']
+scoring = ['precision_macro', 'recall_macro', 'f1']
 scores = cross_validate(clf, X_train, Y_train, scoring=scoring,cv=5, return_train_score=False)
 sorted(scores.keys())
 print scores['test_precision_macro']  
 print scores['test_recall_macro']
+print scores['test_f1']
 
